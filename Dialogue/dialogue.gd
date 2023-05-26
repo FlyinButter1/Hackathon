64abad27c19d28b3cd0a0a3754ad3a1b32ae3de4
Gabriel Lisiecki <gabigabiec@gmail.com>
2023-05-26 21:37:37 +0200

    Dialogue  system
    full dialogue system and viewport scaling fix

---
+++ Dialogue/dialogue.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+@export_file('*.json') var file_path
+
+@onready var background = $NinePatchRect
+
+var dialogue = []
+var current_dialogue_id = 0
+
+signal dialogue_edned
+	
+func _ready():
+	start()
+
+func start():
+	visible = true
+	dialogue = load_dialogue()	
+	$NinePatchRect/name.text = dialogue[0]['name']
+	$NinePatchRect/text.text = dialogue[0]['text']
+	$NinePatchRect/TextureRect.texture = load(dialogue[0]['img'])
+	
+	
+func load_dialogue():
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var json = JSON.new()
+	json.parse(file.get_as_text())
+	return json.get_data()
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+
+func next_script():
+	current_dialogue_id +=1
+	
+	if current_dialogue_id == len(dialogue):
+		visible = false
+		emit_signal('dialogue_edned')
+		return
+	$NinePatchRect/name.text = dialogue[current_dialogue_id]['name']
+	$NinePatchRect/text.text = dialogue[current_dialogue_id]['text']
+	$NinePatchRect/TextureRect.texture = load(dialogue[current_dialogue_id]['img'])
