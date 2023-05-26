6757d4b0d90e73ab7325f8b8e4bde2919932b2c4
Gabriel Lisiecki <gabigabiec@gmail.com>
2023-05-27 00:55:48 +0200

    Player full

--- Player/player.gd
+++ Player/player.gd
@@ -1,24 +1,22 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 165
-@export var ACCELERATION = 80
-@export var FRICTION = 150
-@export var ROLL_SPEED = 175
+@export var MAX_SPEED = 100
+@export var ACCELERATION = 50
+@export var FRICTION = 100
 
 @onready var animationTree = $AnimationTree
 @onready var animationTreePlayback = animationTree.get('parameters/playback')
-@onready var hitbox = $Hitbox
+@onready var hitbox = $HitboxAnchor/Hitbox
 var input_vector: Vector2 = Vector2.ZERO
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/test.dialogue"))
+	if event.is_action_pressed("attack"):
+		state = ATTACK
 
 var can_attack: bool
 
 enum {
 	ATTACK,
-	ROLL,
 	MOVE
 }
 
@@ -31,29 +29,35 @@ func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state()
-			
+		ATTACK:
+			attack_state()
 
 func move_state():
-			input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-			input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-			
-			if input_vector != Vector2.ZERO:
-				animationTree.set('parameters/Idle/blend_position', input_vector)
-				animationTree.set('parameters/Attack/blend_position', input_vector)
-				animationTree.set('parameters/Run/blend_position', input_vector)
-				animationTree.set('parameters/Roll/blend_position', input_vector)
-				hitbox.knockback_vector = input_vector.normalized()
-				animationTreePlayback.travel('Run')
-				velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION)
-			else:
-				animationTreePlayback.travel('Idle')
-				velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-				
-
-			move_and_slide()
-
-
+	print('move')
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
+	if input_vector != Vector2.ZERO:
+		animationTree.set('parameters/Idle/blend_position', input_vector)
+		animationTree.set('parameters/Attack/blend_position', input_vector)
+		animationTree.set('parameters/Run/blend_position', input_vector)
+		animationTree.set('parameters/Roll/blend_position', input_vector)
+		hitbox.knockback_vector = input_vector.normalized()
+		animationTreePlayback.travel('Run')
+		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION)
+	else:
+		animationTreePlayback.travel('Idle')
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+		
+
+	move_and_slide()
+
+
+func attack_state():
+	animationTreePlayback.travel('Attack')
+	
+func attack_end():
+	state = MOVE
 
 
 func _on_hurtbox_death():
