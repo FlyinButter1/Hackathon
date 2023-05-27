cd4570799a0cefb91c8ed98c5dd8f2a28c1d6cbc
Gabriel Lisiecki <gabigabiec@gmail.com>
2023-05-27 10:41:47 +0200

    ostatnie poprawki

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -4,15 +4,15 @@ extends Control
 
 var next_scene
 func change_scene(next_scene_path):
-	animationPlayer.play("FadeOut")
 	next_scene = next_scene_path
+	animationPlayer.play("FadeOut")
+	
 	
 func fadeoutfinished():
-	print('ą')
 	get_tree().change_scene_to_file(next_scene)
 
 func _on_start_button_pressed():
-	change_scene("res://world1.tscn")
+	change_scene("res://worldProps/world1.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
